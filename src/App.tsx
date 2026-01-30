1fde103cd71c1735c940e5eb5a01a5b3d39881b1
Edson Rodrigues <143434065+ImaCod3r@users.noreply.github.com>
2026-01-30 22:59:17 +0000

    setting up relative paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Pages
+import LandingPage from "@/pages/LandingPage";
+
 function App() {
     return (
-        <>
-        </>
+        <Router>
+            <Routes>
+                <Route path="/" element={ <LandingPage /> } />
+            </Routes>
+        </Router>
     )
 }
 
